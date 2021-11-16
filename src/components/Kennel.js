3be9b38ac83c1a726336c90bc92f346716975409
warrior14 <ironaddicts14@gmail.com>
2021-11-16 14:42:25 -0600

    created corressponding files

---
+++ src/components/Kennel.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./Kennel.css"
+
+export const Kennel = () => {
+    <>
+        <h2>Nashville Kennels</h2>
+            <small>Loving care when you're not there!</small>
+                <address>
+                    <div>Visit us at the Nashville North Location</div>
+                    <div>500 Puppy Way</div>
+                </address>
+    </>
+}

601bca6b8a9baa4658c88472ca0374b62d5fce48
warrior14 <ironaddicts14@gmail.com>
2021-11-18 21:03:34 -0600

    refactored the routes to newest version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Kennel} from './components/Kennel.js';
+import { Kennel } from './components/Kennel.js';
 import { BrowserRouter as Router } from "react-router-dom";
 
 

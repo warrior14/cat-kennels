5dc74619109fc920725e5f6f2f66807c600b0b65
warrior14 <ironaddicts14@gmail.com>
2021-11-25 21:26:02 -0600

    refactored animal card so data displays

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -5,16 +5,24 @@ import { AnimalCard } from "./animal/AnimalCard.js";
 import { CustomerCard } from "./customer/CustomerCard.js";
 import { LocationCard } from "./location/LocationCard.js";
 import { EmployeeCard } from "./employee/EmployeeCard.js";
+import { AnimalProvider } from "./animal/AnimalProvider.js";
+import { AnimalList } from "./animal/AnimalList.js";
+
+// every component must be inside 
+
 
 export const ApplicationViews = () => {
     return (
         <Routes>
                 {/* render the location list when http://locahost:3000/ */}
                 <Route exact path="/" element={<Home />}/>
-                        
-                {/* render the animal list when http:localhost:3000/animals */}
-                <Route path="/animals" element={<AnimalCard />}/>         
 
+                {/* render the animal list when http:localhost:3000/animals */}
+                <Route path="/animals" element={
+                    <AnimalProvider>
+                        <AnimalCard />
+                    </AnimalProvider>
+                }/>         
                  {/* render the animal list when http:localhost:3000/animals */}
                  <Route path="/customers" element={<CustomerCard />}/>   
 
--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,14 +1,15 @@
-import React from "react";
-import "./Animal.css";
+// import React from "react";
+// import "./Animal.css";
 
 
-export const AnimalCard = () => {
-    return <>
-            <section className="animal">
-                <h3 className="animal_name">Doodles</h3>
-                <div className="animal_breed">Breed: Poodle</div>
-            </section>
+// export const AnimalCard = () => {
+//     return <>
+//             <section className="animal">
+//                 <h3 className="animal_name">Doodles</h3>
+//                 <div className="animal_breed">Breed: Poodle</div>
+//             </section>
 
-            </>
+//             </>
 
-}
+// }
+
